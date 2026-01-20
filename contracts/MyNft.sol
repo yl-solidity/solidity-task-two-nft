5e2e11e918d06607e9bc2dfeae3e90bdcb9175c8
Administrator <744890753@qq.com>
2026-01-20 19:57:59 +0800

    refactor: 解决npx hardhat compile报错
    - 解决emit调用sender参数，timestamp参数错误嗯他
    - 解决_nextTokenId类型和下面使用的不符问题——统一采用uint256
    
    - 解决删除Counters后的问题：直接使用unit256，并修改对应调用

--- contracts/MyNft.sol
+++ contracts/MyNft.sol
@@ -6,8 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counters;
-    Counters.Counter private _tokenIdCounter;
+   uint256 private _nextTokenId = 0;  // 或 1，从0或1开始
 
     event Minted(address sender, address to, uint256 tokenId, uint256 timestamp);
 
@@ -19,22 +18,22 @@ contract MyNFT is ERC721, ERC721URIStorage, Ownable {
      * 安全铸造
      */
     function safeMint(address to, string memory uri) public onlyOwner returns(uint256){
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
+        uint256 tokenId = _nextTokenId;
+        _nextTokenId++;
         _safeMint(to,tokenId);
         _setTokenURI(tokenId, uri);
-         emit Minted(sender, to, tokenId, timestamp);
+         emit Minted(msg.sender, to, tokenId, block.timestamp);
         return tokenId;
     }
     /**
      * 非安全铸造函数
      */
     function mint(address recipient, string memory _tokenURI) public onlyOwner returns(uint256) {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
+        uint256 tokenId = _nextTokenId;
+        _nextTokenId++;
         _mint(recipient, tokenId);
         _setTokenURI(tokenId, _tokenURI);
-        emit Minted(sender, to, tokenId, timestamp);
+        emit Minted(msg.sender, recipient, tokenId, block.timestamp);
         return tokenId;
     }
 
@@ -44,7 +43,7 @@ contract MyNFT is ERC721, ERC721URIStorage, Ownable {
     function mintBatch(address[] memory to, string[] memory uris) public onlyOwner {
         require(to.length == uris.length, "Arrays length mismatch");
 
-        uint16 length = to.length;
+        uint256 length = to.length;
         for(uint256 i =0; i<length; i++){
             mint(to[i], uris[i]);
         }
@@ -53,7 +52,7 @@ contract MyNFT is ERC721, ERC721URIStorage, Ownable {
      * 获取当前tokenid   
      */
     function getCurrentTokenId() public view returns(uint256) {
-        return _tokenIdCounter.current();
+        return _nextTokenId;
     }
     /**
      * 重写获取 NFT 的元数据链接函数，因为ERC721, ERC721URIStorage都有tokenURI函数
